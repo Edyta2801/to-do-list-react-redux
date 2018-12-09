c2b3af64a9b556ba827f74f9c952feaeaa707ab0
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-09 18:42:32 +0100

    Updates

---
+++ precache-manifest.d5498eccb6f01d44dfef5d0189956fa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83001a90d86d961347cb",
+    "url": "/static/js/main.83001a90.chunk.js"
+  },
+  {
+    "revision": "e85cf93364db960a864e",
+    "url": "/static/js/1.e85cf933.chunk.js"
+  },
+  {
+    "revision": "83001a90d86d961347cb",
+    "url": "/static/css/main.82dddc21.chunk.css"
+  },
+  {
+    "revision": "459a2827cf904a7a6869a895f655d50d",
+    "url": "/index.html"
+  }
+];
