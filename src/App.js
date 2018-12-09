2d1ce7371a75f6bea765a3d8137a20565ae0b0b3
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-09 10:29:16 +0100

    delete task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ToDoList from './components/ToDoList'
+import AppBar from './components/AppBar'
+
 
 class App extends Component {
   render() {
     return (
       <div>
+        <AppBar/>
 <ToDoList/>
       </div>
     );
---
+++ src/components/AppBar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux';
+import MuiAppBar from 'material-ui/AppBar'
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import IconButton from 'material-ui/IconButton';
+import {logOut} from "../state/auth";
+
+const AppBar = (props) => {
+    return (
+        <div>
+            <MuiAppBar
+                onLeftIconButtonClick={props.open}
+                iconElementRight={<IconButton><NavigationClose/></IconButton>}
+                onRightIconButtonClick={props.logOut}
+
+            />
+        </div>
+    );
+}
+
+
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        logOut: () => dispatch(logOut())
+    }
+
+    };
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AppBar);
